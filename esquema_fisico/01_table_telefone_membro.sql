4aa0464539f6875049feff494fcf708968d931f6
Thiago Albino <90213963+Thi08@users.noreply.github.com>
2023-08-02 17:34:55 -0300

    Update 01_table_telefone_membro.sql

--- esquema_fisico/01_table_telefone_membro.sql
+++ esquema_fisico/01_table_telefone_membro.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS telefone_membro (
-    identidade int,
+    membro_identidade int,
     DDI varchar(3),
     DDD varchar(2),
     Telefone varchar(9),
-    PRIMARY KEY (identidade)
+    PRIMARY KEY (membro_identidade)
 );
