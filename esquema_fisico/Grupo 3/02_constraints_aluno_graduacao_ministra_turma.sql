ddb5829c8fb9b9dd1d799d7d2e363130b4e3c752
Pietro-MM <77638940+Pietro-MM@users.noreply.github.com>
2023-08-02 11:59:26 -0300

    Update 02_constraints_aluno_graduacao_ministra_turma.sql

--- esquema_fisico/Grupo 3/02_constraints_aluno_graduacao_ministra_turma.sql
+++ esquema_fisico/Grupo 3/02_constraints_aluno_graduacao_ministra_turma.sql
@@ -2,4 +2,4 @@ ALTER TABLE andifes.aluno_graduacao_ministra_turma
     ADD CONSTRAINT FK_turma_ministrada FOREIGN KEY (sigla_turma) REFERENCES andifes.turma(sigla)
     
 ALTER TABLE andifes.aluno_graduacao_ministra_turma
-    ADD CONSTRAINT FK_aluno_ministrante FOREIGN KEY REFERENCES andifes.aluno_graduacao(identidade_aluno_graduacao)
+    ADD CONSTRAINT FK_aluno_ministrante FOREIGN KEY (aluno_ministrante) REFERENCES andifes.aluno_graduacao(identidade_aluno_graduacao)
