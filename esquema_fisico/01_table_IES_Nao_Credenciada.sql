ca8165beadc367008bb62e6e94ca013fdb3b4062
Dezim <52601996+andresouza98@users.noreply.github.com>
2023-08-02 17:08:01 -0300

    Update 01_table_IES_Nao_Credenciada.sql

--- esquema_fisico/01_table_IES_Nao_Credenciada.sql
+++ esquema_fisico/01_table_IES_Nao_Credenciada.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS andifes.IES_Nao_Credenciada(
-    Sigla varchar(255) PRIMARY KEY,
-    Codigo_Cred integer
+    IES-Sigla varchar(20) PRIMARY KEY,
+    Codigo_Cred INTEGER
 );
