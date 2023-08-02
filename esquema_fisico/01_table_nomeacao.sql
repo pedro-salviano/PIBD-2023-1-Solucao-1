cc1dd45132817a79fcdc0cef3edc331694f9130b
Thiago Albino <90213963+Thi08@users.noreply.github.com>
2023-08-02 17:30:06 -0300

    Update 01_table_nomeacao.sql

--- esquema_fisico/01_table_nomeacao.sql
+++ esquema_fisico/01_table_nomeacao.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS nomeacao (
     data_nomeacao date,
-    reitoria_Inicio_Periodo date,
+    Inicio_Periodo date,
     coordenador_administrativo_identidade int,
-    PRIMARY KEY (data_nomeacao, reitoria_Inicio_Periodo)
+    PRIMARY KEY (data_nomeacao, Inicio_Periodo, coordenador_administrativo_identidade)
 );
