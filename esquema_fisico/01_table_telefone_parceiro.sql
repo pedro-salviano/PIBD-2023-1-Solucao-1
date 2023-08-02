bad972e1c01fa0558da5867eecb20bbc56ed442f
viniciusog <viniciusogbr@gmail.com>
2023-08-02 17:23:31 -0300

    Constraint UNIQUE removido

--- esquema_fisico/01_table_telefone_parceiro.sql
+++ esquema_fisico/01_table_telefone_parceiro.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS Telefone_Parceiro(
 	DDI varchar(4) NOT NULL,
 	DDD varchar(2) NOT NULL,
 	Telefone varchar(11) NOT NULL,
-	Parceiro_Id INTEGER,
-	CONSTRAINT telefone_parceiro_unico UNIQUE (DDI, DDD, Telefone)
+	Parceiro_Id INTEGER
 );
