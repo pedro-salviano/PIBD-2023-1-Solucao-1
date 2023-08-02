bc2be358dd7fc35d9b87e9782f95e1725bc930e3
IvanCalvo <ivandc1998.ic@gmail.com>
2023-08-02 17:01:08 -0300

    update constraints

--- esquema_fisico/02_constraints_ies.sql
+++ esquema_fisico/02_constraints_ies.sql
@@ -1,2 +1,2 @@
 ALTER TABLE andifes.Membro_institucional
-ADD CONSTRAINT FK_Endereco_ID FOREIGN KEY (FK_Endereco_ID) REFERENCES andifes.Endereco (Endereco_ID);
+ADD CONSTRAINT FK_Endereco_ID FOREIGN KEY (Endereco_ID) REFERENCES andifes.Endereco (Endereco_ID);
--- esquema_fisico/02_constraints_membro_institucional.sql
+++ esquema_fisico/02_constraints_membro_institucional.sql
@@ -1,2 +1,2 @@
 ALTER TABLE andifes.Membro_institucional
-ADD CONSTRAINT FK_Membro_Academico_Identidade FOREIGN KEY (FK_Membro_Academico_Identidade) REFERENCES andifes.Membro_academico (Identidade);
+ADD CONSTRAINT FK_Membro_Academico_Identidade FOREIGN KEY (Membro_Academico_Identidade) REFERENCES andifes.Membro_academico (Identidade);
