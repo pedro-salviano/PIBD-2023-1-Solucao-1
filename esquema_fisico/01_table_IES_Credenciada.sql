78a18a02bf2685d32270298402bc1aad341ce6a6
Dezim <52601996+andresouza98@users.noreply.github.com>
2023-08-01 22:41:40 -0300

    Update 01_table_IES_Credenciada.sql

--- esquema_fisico/01_table_IES_Credenciada.sql
+++ esquema_fisico/01_table_IES_Credenciada.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS andifes.IES_Credenciada(
-    Sigla VARCHAR(10) REFERENCES IES(Sigla),
+    Sigla VARCHAR(10),
     N_Esp_Cred INTEGER,
     PRIMARY KEY (Sigla, N_Esp_Cred)
 );
