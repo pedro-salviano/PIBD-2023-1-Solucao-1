2aaf4745c7d23ef6dee33b18b480d8f7a9442458
Marciel Almeida <marciel.almeida@monitoratec.com.br>
2023-08-02 16:24:46 -0300

    commits grupo 4 (tabelas apenas)

---
+++ 01_table_repositorio.sql
@@ -0,0 +1,8 @@
+CREATE TABLE repositorio(
+    codigo_rep INTEGER UNIQUE NOT NULL
+    link_ementa VARCHAR(200),
+    data_fim DATE, 
+  	referencia VARCHAR(200),
+  	nome_completo VARCHAR(200)
+  PRIMARY Key(codigo_rep)
+)
