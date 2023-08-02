07a4697c8f9b205860e7e332b6a27b3afce97fff
viniciusog <viniciusogbr@gmail.com>
2023-08-02 12:39:26 -0300

    Table Endereco_Parceiro

---
+++ esquema_fisico/01_table_endereco_parceiro.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Endereco_Parceiro(
+    Id SERIAL PRIMARY KEY,
+	Codigo_postal VARCHAR(50) NOT NULL,
+	Pais VARCHAR(50) NOT NULL,
+	Bairro VARCHAR(50) NOT NULL,
+	Rua VARCHAR(50) NOT NULL,
+	Numero VARCHAR(5) NOT NULL,
+	Cidade VARCHAR(50),
+	Estado VARCHAR(50),
+	Complemento VARCHAR(50),
+    Parceiro_Id INTEGER,
+);
