7e6f227e99f9cca2832ed2d6f5c82a6f4a52503d
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-04 19:39:22 -0300

    Update 01_table_especialista_produz_material.sql

--- esquema_fisico/01_table_especialista_produz_material.sql
+++ esquema_fisico/01_table_especialista_produz_material.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS especialista_produz_material(
     produz_material_id SERIAL CONSTRAINT PK_especialista_produz_material PRIMARY KEY,
-    especialista_id INTEGER,
+    membro_academico_id INTEGER,
     material_id INTEGER
 );
