3fd2168bd167a1bccf3e74c80893a8e6d584e287
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 19:27:44 -0300

    Update 02_constraints_alunoProfessorISF.sql

--- esquema_fisico/02_constraints_alunoProfessorISF.sql
+++ esquema_fisico/02_constraints_alunoProfessorISF.sql
@@ -1,4 +1,4 @@
-ALTER TABLE IF NOT EXISTS andifes.alunoProfessorISF
+ALTER TABLE IF NOT EXISTS alunoProfessorISF
     ADD CONSTRAINT FK_membro_academico_id 
             FOREIGN KEY (id)
             REFERENCES membro_academico(id);
