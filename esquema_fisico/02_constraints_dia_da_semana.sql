5c23fb4decdd752714095c231c65cedcbf123537
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 19:28:45 -0300

    Update 02_constraints_dia_da_semana.sql

--- esquema_fisico/02_constraints_dia_da_semana.sql
+++ esquema_fisico/02_constraints_dia_da_semana.sql
@@ -1,11 +1,11 @@
-ALTER TABLE andifes.dia_da_semana
+ALTER TABLE dia_da_semana
     ADD CONSTRAINT fk_idioma
         FOREIGN KEY(idioma)
         REFERENCES trilhas_ofertadas(idioma)
                 ON DELETE CASCADE
                 ON UPDATE CASCADE;
 
-ALTER TABLE andifes.dia_da_semana
+ALTER TABLE dia_da_semana
     ADD CONSTRAINT fk_codigo
         FOREIGN KEY(codigo)
         REFERENCES componenteCurricular(codigo)
