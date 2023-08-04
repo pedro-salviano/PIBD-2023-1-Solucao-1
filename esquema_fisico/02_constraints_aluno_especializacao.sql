9e783c5abed27cc8ea966e1979ea1328bf1eef20
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 19:28:00 -0300

    Update 02_constraints_aluno_especializacao.sql

--- esquema_fisico/02_constraints_aluno_especializacao.sql
+++ esquema_fisico/02_constraints_aluno_especializacao.sql
@@ -1,4 +1,4 @@
-ALTER TABLE andifes.aluno_especializacao
+ALTER TABLE aluno_especializacao
         ADD CONSTRAINT FK_aluno_prof_id
             FOREIGN KEY (id)
             REFERENCES aluno_professor_ISF(id); 
