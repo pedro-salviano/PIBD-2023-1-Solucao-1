e938ea329d9e94aa09d4d7a8c7f697abd1354f83
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 19:40:56 -0300

    Update 01_table_aluno_especializacao.sql

--- esquema_fisico/01_table_aluno_especializacao.sql
+++ esquema_fisico/01_table_aluno_especializacao.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS aluno_especializacao(
-    id SERIAL NOT NULL,
     aluno_professor_isf_id INTEGER NOT NULL, 
     DataIngresso DATE NOT NULL,
     DataConclusao DATE,
     DiplomaFile VARCHAR(200),
     Titulacao VARCHAR(200),
     RegistroMinistrante VARCHAR(200),
-    PRIMARY KEY (id)
+    PRIMARY KEY (aluno_professor_isf_id)
 )
