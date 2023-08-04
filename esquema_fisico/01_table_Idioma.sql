bbbe1e8edaad2230bcfb8f615ada9df4a1b14285
0Notzura <111460844+0Notzura@users.noreply.github.com>
2023-08-04 18:03:24 -0300

    Update 01_table_Idioma.sql

--- esquema_fisico/01_table_Idioma.sql
+++ esquema_fisico/01_table_Idioma.sql
@@ -2,5 +2,5 @@ CREATE TABLE Idioma (
   Identidade VARCHAR(11) ,
   Lingua VARCHAR(50) not NULL,
   DeclaracaoProeficiencia VARCHAR(255) not NULL,
-  Idioma_id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY
 );
