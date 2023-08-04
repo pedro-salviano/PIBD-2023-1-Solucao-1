bf891a99d2dd8b6963018f9f67c6fb1e219306a2
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 19:19:51 -0300

    Update 01_table_aluno_especializacao.sql
    Ajustes conforme solicitado pelo Vini

--- esquema_fisico/01_table_aluno_especializacao.sql
+++ esquema_fisico/01_table_aluno_especializacao.sql
@@ -1,9 +1,10 @@
-CREATE TABLE IF NOT EXISTS andifes.aluno_especializacao(
-    identidade INTEGER NOT NULL, 
+CREATE TABLE IF NOT EXISTS aluno_especializacao(
+    id SERIAL NOT NULL,
+    aluno_professor_isf_id INTEGER NOT NULL, 
     DataIngresso DATE NOT NULL,
     DataConclusao DATE,
     DiplomaFile VARCHAR(200),
     Titulacao VARCHAR(200),
     RegistroMinistrante VARCHAR(200),
-    PRIMARY KEY (identidade)
+    PRIMARY KEY (aluno_professor_isf_id)
 )
