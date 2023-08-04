395ea9137a6cd388d8873737cef1e55ae9cb42bb
0Notzura <111460844+0Notzura@users.noreply.github.com>
2023-08-04 20:26:50 -0300

    Update 01_table_especialista.sql

--- esquema_fisico/01_table_especialista.sql
+++ esquema_fisico/01_table_especialista.sql
@@ -1,21 +1,10 @@
-CREATE TABLE especialista (
-  membro_academico_id INTEGER NOT NULL INTEGER PRIMARY KEY,
-  IES_Sigla VARCHAR(10) REFERENCES IES(IES_Sigla),
-  Biodata TEXT,
-  Dispo_Ministrar INTEGER(2),
-  Titulacao VARCHAR(100) NOT NULL,
-  Dispo_Ori_Esp INTEGER(2),
-  Vinculo_File VARCHAR(255) NOT NULL,
-  Curriculo VARCHAR(255) NOT NULL,
-  Data_Credenciamento DATE not NULL,
-  Disponibilidade_Autor BOOLEAN not NULL,
-  Reset_Need BOOLEAN,
-  Diploma_File VARCHAR(255) NOT NULL,
-  Link_Cnpq VARCHAR(255) NOT NULL,
-  Poca_File VARCHAR(255) NOT NULL,
-  Registro_Autoria VARCHAR(255),
-  Registro_Ministrante VARCHAR(255),
-  registroMinistrante VARCHAR(100),
-  Atuacao_IsF_Anterior BOOLEAN
-  
+CREATE TABLE IF NOT EXISTS especialista(
+    membro_academico_id INTEGER NOT NULL,
+    Biodata VARCHAR(200), 
+    DispoMinistrar BOOLEAN, 
+    LinkCnpq VARCHAR(200), 
+    PocaFile VARCHAR(200), 
+    RegistroAutoria VARCHAR(200), 
+    RegistroMinistrante VARCHAR(200),
+    PRIMARY KEY (membro_academico_id)
 );
