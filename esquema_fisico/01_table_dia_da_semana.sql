c8a9dccfee4ebb9c56febd9f148f6c6346cb33a7
0Notzura <111460844+0Notzura@users.noreply.github.com>
2023-08-04 18:04:27 -0300

    Update 01_table_dia_da_semana.sql

--- esquema_fisico/01_table_dia_da_semana.sql
+++ esquema_fisico/01_table_dia_da_semana.sql
@@ -2,5 +2,5 @@ CREATE TABLE Dia_da_semana (
   Idioma VARCHAR(50) REFERENCES Idioma(Lingua),
   Codigo VARCHAR(20),
   dia VARCHAR(20),
-  Dia_da_semana_id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY
 );
