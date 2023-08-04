ba18af354a90b65a0bdca4d110317784bda93267
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-04 19:53:58 -0300

    Update 01_table_membro_academico_telefone.sql
    Should have only one datatype

--- esquema_fisico/01_table_membro_academico_telefone.sql
+++ esquema_fisico/01_table_membro_academico_telefone.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS membro_academico_telefone(
-    id INT SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     membro_academico_id INT NOT NULL, /* Foreign key */
     ddi varchar(3),
     ddd varchar(3),
