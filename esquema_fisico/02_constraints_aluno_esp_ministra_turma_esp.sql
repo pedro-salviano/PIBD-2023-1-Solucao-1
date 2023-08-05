2e46b739900acebe5eaaf9d82090a3723a0458e7
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-05 00:12:08 -0300

    Update 02_constraints_aluno_esp_ministra_turma_esp.sql

--- esquema_fisico/02_constraints_aluno_esp_ministra_turma_esp.sql
+++ esquema_fisico/02_constraints_aluno_esp_ministra_turma_esp.sql
@@ -1,5 +1,5 @@
 ALTER TABLE aluno_esp_cursa_turma_esp
-    ADD CONSTRAINT FK_turma_esp FOREIGN KEY (turma_especializacao_id) REFERENCES turma_especializacao(id)
+    ADD CONSTRAINT FK_turma_esp FOREIGN KEY (turma_especializacao_id) REFERENCES turma_especializacao(id);
     
 ALTER TABLE aluno_esp_cursa_turma_esp
-    ADD CONSTRAINT FK_aluno_esp FOREIGN KEY (aluno_professor_isf_id) REFERENCES aluno_especializacao(aluno_professor_isf_id)
+    ADD CONSTRAINT FK_aluno_esp FOREIGN KEY (aluno_professor_isf_id) REFERENCES aluno_especializacao(aluno_professor_isf_id);
