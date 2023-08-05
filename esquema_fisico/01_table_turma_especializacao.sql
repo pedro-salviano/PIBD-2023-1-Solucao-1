29e26ccfcb1eacb74b44bd60915f4761e5245a5d
Vinícius Guimarães <vinijipa23@gmail.com>
2023-08-04 22:50:40 -0300

    Update 01_table_turma_especializacao.sql

--- esquema_fisico/01_table_turma_especializacao.sql
+++ esquema_fisico/01_table_turma_especializacao.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS turma_especializacao(
 	Data_Fim DATE,
 	Data_Inicio DATE NOT NULL,
 	Horario_de_ocorrencia VARCHAR(30) NOT NULL,
+	trilha_escolhida_id INT,
 	dia dia_da_semana_enum NOT NULL
 );
