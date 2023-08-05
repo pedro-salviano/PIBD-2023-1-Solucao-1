b1f913dd85b27943c3bc596f2432ca8707e4cd4e
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 22:04:55 -0300

    Update 01_table_dia_da_semana.sql

--- esquema_fisico/01_table_dia_da_semana.sql
+++ esquema_fisico/01_table_dia_da_semana.sql
@@ -1,6 +1,6 @@
 CREATE TABLE if NOT EXISTS dia_da_semana(
     id SERIAL NOT NULL,
-    idioma VARCHAR(20) UNIQUE NOT NULL,
+    trilha_ofertada_id INTEGER UNIQUE NOT NULL,
     codigo varchar(10),
     dia dia_da_semana_enum NOT NULL,
     PRIMARY KEY(id)
