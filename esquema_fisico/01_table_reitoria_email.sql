dab039bba8557a2afbba8d5ff680b6604d22741f
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-04 22:55:53 -0300

    Update 01_table_reitoria_email.sql

--- esquema_fisico/01_table_reitoria_email.sql
+++ esquema_fisico/01_table_reitoria_email.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS reitoria_email(
-    id INTEGER SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     reitoria_id INT NOT NULL, /* Foreign key */
     Email VARCHAR(30),
     PRIMARY KEY (id)
