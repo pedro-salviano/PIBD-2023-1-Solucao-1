7d5622a2c3219ab3de3fbaec6f9d5f6d71c9e14b
pinguim11 <52601915+pinguim11@users.noreply.github.com>
2023-08-04 22:31:25 -0300

    Update 01_table_turma_especializacao.sql
    criação da tabela turma_especializacao

---
+++ esquema_fisico/01_table_turma_especializacao.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS turma_especializacao( 
+	turma_especializacao_id INTEGER CONSTRAINT PK_turma_especializacao PRIMARY KEY,
+	Data_Fim DATE,
+	Data_Inicio DATE NOT NULL,
+	Horario_de_ocorrencia VARCHAR(30) NOT NULL,
+	dia dia_da_semana_enum NOT NULL
+);
