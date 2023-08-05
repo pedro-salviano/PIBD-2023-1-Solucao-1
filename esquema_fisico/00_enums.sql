860c9f2c287ab322064d1d474290abc108a09cbf
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-08-04 22:07:34 -0300

    Update 00_enums.sql

--- esquema_fisico/00_enums.sql
+++ esquema_fisico/00_enums.sql
@@ -1,2 +1,2 @@
-CREATE TYPE if NOT EXISTS tipo_parceiro('ONG', 'Orgão Governamental', 'Empresa Privada')
-CREATE TYPE if NOT EXISTS dia_da_semana_enum('Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado', 'Domingo')
+CREATE TYPE tipo_parceiro AS ENUM ('ONG', 'Orgão Governamental', 'Empresa Privada')
+CREATE TYPE dia_da_semana_enum AS ENUM ('Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado', 'Domingo')
