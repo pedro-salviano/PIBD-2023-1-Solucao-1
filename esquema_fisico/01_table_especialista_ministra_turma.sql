1e01208dfcb043e882ea6cd932d2da780be4cad4
viniciusog <viniciusogbr@gmail.com>
2023-08-04 23:53:17 -0300

    Orientacao ministra turma

--- esquema_fisico/01_table_especialista_ministra_turma.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS especialista_ministra_turma (
-    id SERIAL PRIMARY KEY,
-    membro_academico_id INT,
-    turma_id INT,
-);
--- esquema_fisico/02_constraints_especialista_ministra_turma_esp.sql
+++ esquema_fisico/02_constraints_especialista_ministra_turma_esp.sql
@@ -2,6 +2,4 @@ ALTER TABLE especialista_ministra_turma
 ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) REFERENCES especialista (membro_academico_id);
 
 ALTER TABLE especialista_ministra_turma
-ADD CONSTRAINT FK_Turma FOREIGN KEY (turma_id) REFERENCES turma (id);
-
-AQUII
+ADD CONSTRAINT FK_Turma_esp FOREIGN KEY (turma_especializacao_id) REFERENCES turma_especializacao (id);
