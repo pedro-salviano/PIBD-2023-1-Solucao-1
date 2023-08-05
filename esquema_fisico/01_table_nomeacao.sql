580f9fea5123b9cec5fd40f2b0bdead3deda4fe0
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-04 23:58:40 -0300

    Update 01_table_nomeacao.sql

--- esquema_fisico/01_table_nomeacao.sql
+++ esquema_fisico/01_table_nomeacao.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS nomeacao (
 	id SERIAL PRIMARY KEY,
     data date,
     reitoria_id integer,
-    coordenador_administrativo_identidade int
+    coordenador_administrativo_id int
 );
