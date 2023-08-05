eec8f34d7a1fa01b003ca97b77f72bf56b043500
Vinícius Guimarães <vinijipa23@gmail.com>
2023-08-04 22:32:49 -0300

    Update 02_constraints_membro_institucional.sql

--- esquema_fisico/02_constraints_membro_institucional.sql
+++ esquema_fisico/02_constraints_membro_institucional.sql
@@ -1,2 +1,2 @@
-ALTER TABLE andifes.Membro_institucional
-ADD CONSTRAINT FK_membro_academico_identidade FOREIGN KEY (membro_academico_identidade) REFERENCES andifes.membro_academico (identidade);
+ALTER TABLE Membro_institucional 
+ADD CONSTRAINT FK_membro_academico_identidade FOREIGN KEY (membro_academico_identidade) REFERENCES membro_academico (identidade);
