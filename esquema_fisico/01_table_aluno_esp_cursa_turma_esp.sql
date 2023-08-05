395018da39f6c9e86681c2fee0dd1316d0534807
Pedro Salviano <102177946+pedro-salviano@users.noreply.github.com>
2023-08-05 00:11:26 -0300

    Update 01_table_aluno_esp_cursa_turma_esp.sql

--- esquema_fisico/01_table_aluno_esp_cursa_turma_esp.sql
+++ esquema_fisico/01_table_aluno_esp_cursa_turma_esp.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS aluno_esp_cursa_turma_esp (
     status_inscricao status_inscricao_enum NOT NULL,
     nota_final DECIMAL
     aluno_professor_isf_id INT,
-    turma_especializacao_id INT,
+    turma_especializacao_id INT
 );
