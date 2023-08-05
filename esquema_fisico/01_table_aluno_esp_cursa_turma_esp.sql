a419c7e5643ee0ba7677ff2c539974287b795744
viniciusog <viniciusogbr@gmail.com>
2023-08-05 00:10:21 -0300

    Aluno especializacao cursa turma especializacao e constraints'

--- esquema_fisico/01_table_aluno_esp_cursa_turma_esp.sql
+++ esquema_fisico/01_table_aluno_esp_cursa_turma_esp.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS aluno_esp_cursa_turma_esp (
     id SERIAL PRIMARY KEY,
     status_inscricao status_inscricao_enum NOT NULL,
-    nota_final DECIMAL,
-    PRIMARY KEY (aluno_professor_isf_id)
+    nota_final DECIMAL
+    aluno_professor_isf_id INT,
+    turma_especializacao_id INT,
 );
