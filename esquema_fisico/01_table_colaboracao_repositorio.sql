a8b1d06879c9eb16ede22cb284820870072216db
Pedro Salviano <pedrosalviano3@gmail.com>
2023-08-30 16:38:09 -0300

    update der and repositorio.sql

--- esquema_fisico/01_table_colaboracao_repositorio.sql
+++ esquema_fisico/01_table_colaboracao_repositorio.sql
@@ -1,7 +1,6 @@
 CREATE TABLE if NOT EXISTS colaboracao_repositorio(
     id SERIAL NOT NULL,
-    especialista_id INTEGER NOT NULL,
-    orientado_id INTEGER NOT NULL,
+    orientacao_id INTEGER NOT NULL,
     codigo_rep INTEGER NOT NULL, 
     PRIMARY KEY(id)
 )
