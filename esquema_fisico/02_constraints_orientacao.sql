6d364032640ffe33015e13142589d09b9da3e670
Pedro Salviano <pedrosalviano3@gmail.com>
2023-08-30 23:28:48 -0300

    update file names and minor corrections

--- esquema_fisico/02_constraints_orientacao.sql
+++ esquema_fisico/02_constraints_orientacao.sql
@@ -2,4 +2,4 @@ ALTER TABLE orientacao
 ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) REFERENCES especialista(membro_academico_id);
 
 ALTER TABLE orientacao
-ADD CONSTRAINT FK_aluno_esp FOREIGN KEY (aluno_professor_isf_id) REFERENCES aluno_especializacao(professor_isf_id);
+ADD CONSTRAINT FK_aluno_professor_isf FOREIGN KEY (aluno_professor_isf_id) REFERENCES aluno_professor_isf(membro_academico_id);
