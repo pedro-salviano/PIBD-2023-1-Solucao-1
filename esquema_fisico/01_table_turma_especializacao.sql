f0654262c62e54d163f8a7c4a405673db4a738c5
Pietro-MM <77638940+Pietro-MM@users.noreply.github.com>
2023-08-31 13:01:34 -0300

    Atualizações Solicitadas

--- esquema_fisico/01_table_turma_especializacao.sql
+++ esquema_fisico/01_table_turma_especializacao.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS turma_especializacao( 
-	id INTEGER CONSTRAINT PK_turma_especializacao PRIMARY KEY,
+	id SERIAL NOT NULL,
 	Data_Fim DATE,
 	Data_Inicio DATE NOT NULL,
 	trilha_pertencente_id INT,
-	componente_curricular_id INT
+	componente_curricular_id INT,
+	PRIMARY KEY(id)
 );
--- esquema_fisico/02_constraints_material.sql
+++ esquema_fisico/02_constraints_material.sql
@@ -1,2 +1,2 @@
-ALTER TABLE material ADD CONSTRAINT FK_componente_curricular FOREIGN KEY (componente_curricular_codigo) REFERENCES componente_curricular(codigo);
+ALTER TABLE material ADD CONSTRAINT FK_componente_curricular FOREIGN KEY (id_componente_curricular) REFERENCES componente_curricular(id_componente_curricular);
 ALTER TABLE material ADD CONSTRAINT FK_material_anterior FOREIGN KEY (material_anterior_id) REFERENCES material(material_id);
