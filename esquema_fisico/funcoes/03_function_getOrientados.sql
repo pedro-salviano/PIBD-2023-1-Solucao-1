85f86596c22ae3b65f9748b0e02cde18a9cddd69
Daniel Hiroyuki Watanabe <35534711+dahiwas@users.noreply.github.com>
2023-09-04 16:52:14 -0300

    Alteração no teste do script

--- esquema_fisico/funcoes/03_function_getOrientados.sql
+++ esquema_fisico/funcoes/03_function_getOrientados.sql
@@ -4,55 +4,7 @@
 --É possível escrever nos dois inputs, será filtrado pelo mesmo
 --Para funcionar necessita apenas da existencia da table orientacao/membro_academico e da view vOrientados
 
--------------------------Tables necessárias:
---membro_academico
-/*
-CREATE TABLE IF NOT EXISTS membro_academico(
-    id SERIAL NOT NULL,
-    identidade INTEGER NOT NULL UNIQUE, 
-    nacionalidade varchar(20) NOT NULL,
-    pais_de_residencia varchar(19) NOT NULL,
-    nome_completo varchar(70) NOT NULL,
-    data_nascimento DATE NOT NULL,
-    nome_da_mae varchar(70),
-    genero varchar(20),
-    PRIMARY KEY (id)
-);
-*/
---orientacao
-/*
-CREATE TABLE IF NOT EXISTS orientacao (
-    id SERIAL PRIMARY KEY,
-    membro_academico_id INT, /*chave primária especialista*/
-    aluno_professor_isf_id INT, /*chave primária aluno esp */
-    data_inicial DATE,
-    data_final DATE
-);
-ALTER TABLE orientacao 
-ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) REFERENCES membro_academico(identidade);
-*/
-
---View necessária:
---vOrientados:
-/*
-CREATE OR REPLACE VIEW vOrientados AS
-SELECT 
-    o.identidade AS orientador_id,
-    o.nome_completo AS orientador_nome,
-    a.identidade AS orientado_id,
-    a.nome_completo AS orientado_nome,
-	CASE WHEN oe.data_final IS NULL OR oe.data_final > CURRENT_DATE THEN 'ativo' ELSE 'inativo' END AS status
-FROM 
-    orientacao oe
-LEFT JOIN
-    membro_academico o ON oe.membro_academico_id = o.identidade -- Orientadores
-LEFT JOIN
-    membro_academico a ON oe.aluno_professor_isf_id = a.identidade; -- Orientados
-*/
--------------------------------------------------------------------------------------------------------
---Function getOrientados
-
-CREATE OR REPLACE FUNCTION getOrientados(
+CREATE OR REPLACE FUNCTION get_orientados(
     orientador_input text,
     orientado_input text
 )
@@ -73,11 +25,10 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
-
-
 /*
---Scripts para testar
+
 --Inserindo exemplos de pessoas
+------------Inserção dos membros academicos
 INSERT INTO membro_academico (identidade, nacionalidade, pais_de_residencia, nome_completo, data_nascimento, nome_da_mae, genero)
 VALUES (123456, 'Brasileira', 'Brasil', 'João da Silva', '1990-01-15', 'Mario da Silva', 'Masculino');
 
@@ -99,24 +50,39 @@ VALUES (147258, 'Brasileira', 'Brasil', 'Yukihira Akame', '1990-01-15', 'Paloma 
 INSERT INTO membro_academico (identidade, nacionalidade, pais_de_residencia, nome_completo, data_nascimento, nome_da_mae, genero)
 VALUES (785963, 'Brasileira', 'Brasil', 'Trilecas Alexandria', '1990-01-15', 'Iodeto da Silva', 'Masculino');
 
---Inserindo relações
-INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 23457, NOW());
+------------Inserindo Aluno_Professor_ISF
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), 'algumRA')
+
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), 'algumRA')
+
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), 'algumRA')
+
+------------Inserindo Aluno_especializacao
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), NOW())
+
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), NOW())
+
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), NOW())
+
+------------ Finalmente inserindo as relações
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (89798789, 1593557, NOW());
+VALUES (123456, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), NOW());
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (89798789, 258369, NOW());
+VALUES (123456, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), NOW());
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 147258, NOW());
-
-INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 785963, NOW());
+VALUES (785963, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), NOW());
 
 --Exemplos da function
-SELECT * FROM getOrientados(NULL, NULL); Retorna tudo
-SELECT * FROM getOrientados('João da Silva', NULL); Retorna apenas os orientados com aquele orientador
-SELECT * FROM getOrientados(NULL, 'Yukihira Akame'); Retorna apenas aquele orientado
+SELECT * FROM get_orientados(NULL, NULL); Retorna tudo
+SELECT * FROM get_orientados('João da Silva', NULL); Retorna apenas os orientados com aquele orientador
+SELECT * FROM get_orientados(NULL, 'Hirokawa da Silva'); Retorna apenas aquele orientado
 */
