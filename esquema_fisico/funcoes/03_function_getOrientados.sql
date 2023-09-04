4e60995fb86483b6ad1215f2edab305cec77bcdc
Daniel Hiroyuki Watanabe <35534711+dahiwas@users.noreply.github.com>
2023-09-04 16:54:40 -0300

    Formatação

--- esquema_fisico/funcoes/03_function_getOrientados.sql
+++ esquema_fisico/funcoes/03_function_getOrientados.sql
@@ -2,8 +2,96 @@
 --É possível filtrar todos os orientados do orientador, basta inserir apenas o nome do orientador
 --É possível também verificar quem orienta determinado orientado, basta inserir apenas o nome do orientado
 --É possível escrever nos dois inputs, será filtrado pelo mesmo
---Para funcionar necessita apenas da existencia da table orientacao/membro_academico e da view vOrientados
+--Para funcionar necessita da existencia das tables:
+-- orientacao/membro_academico/aluno_especializacao/aluno_professor_isf/
+-- e da view vOrientados
 
+/*
+-------------------------Tables necessárias:
+-------------------------------Table membro_academico
+
+CREATE TABLE IF NOT EXISTS membro_academico(
+    id SERIAL NOT NULL,
+    identidade INTEGER NOT NULL UNIQUE, 
+    nacionalidade varchar(20) NOT NULL,
+    pais_de_residencia varchar(19) NOT NULL,
+    nome_completo varchar(70) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    nome_da_mae varchar(70),
+    genero varchar(20),
+    PRIMARY KEY (id)
+);
+
+-------------------------------Table orientacao
+
+CREATE TABLE IF NOT EXISTS orientacao (
+    id SERIAL PRIMARY KEY,
+    membro_academico_id INT, /*chave primária especialista*/
+    aluno_professor_isf_id INT, /*chave primária aluno esp */
+    data_inicial DATE,
+    data_final DATE
+);
+ALTER TABLE orientacao 
+ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) 
+REFERENCES membro_academico(identidade);
+
+ALTER TABLE orientacao 
+ADD CONSTRAINT FK_aluno_esp FOREIGN KEY (aluno_professor_isf_id) 
+REFERENCES aluno_especializacao(aluno_professor_isf_id);
+
+-------------------------------Table aluno_especializacao
+
+CREATE TABLE IF NOT EXISTS aluno_especializacao(
+	id SERIAL PRIMARY KEY,
+	aluno_professor_isf_id INTEGER NOT NULL UNIQUE, 
+    DataIngresso DATE NOT NULL,
+    DataConclusao DATE,
+    DiplomaFile VARCHAR(200),
+    Titulacao VARCHAR(200),
+    RegistroMinistrante VARCHAR(200)
+);
+
+ALTER TABLE aluno_especializacao
+        ADD CONSTRAINT FK_aluno_prof_id
+            FOREIGN KEY (aluno_professor_isf_id)
+            REFERENCES aluno_professor_isf(membro_academico_id); 
+
+
+-------------------------------Table aluno_professor_isf
+
+CREATE TABLE IF NOT EXISTS aluno_professor_isf (
+	id SERIAL PRIMARY KEY,
+    membro_academico_id INTEGER UNIQUE NOT NULL,
+    RA VARCHAR(15) NOT NULL
+);		
+ALTER TABLE aluno_professor_isf
+    ADD CONSTRAINT FK_membro_academico 
+	FOREIGN KEY (membro_academico_id) 
+	REFERENCES membro_academico(id);
+
+
+-------------------------------vOrientados
+
+CREATE OR REPLACE VIEW vOrientados AS
+SELECT 
+    o.identidade AS orientador_id,
+    o.nome_completo AS orientador_nome,
+    a.identidade AS orientado_id,
+    a.nome_completo AS orientado_nome,
+	CASE WHEN oe.data_final IS NULL OR oe.data_final > CURRENT_DATE THEN 'ativo' ELSE 'inativo' END AS status
+FROM 
+    orientacao oe
+LEFT JOIN
+    membro_academico o ON oe.membro_academico_id = o.identidade -- Orientadores
+LEFT JOIN
+    aluno_especializacao ae ON oe.aluno_professor_isf_id = ae.aluno_professor_isf_id
+LEFT JOIN
+    aluno_professor_isf ap ON ae.aluno_professor_isf_id = ap.membro_academico_id
+LEFT JOIN
+    membro_academico a ON ap.membro_academico_id = a.id; -- Orientados
+*/
+
+-------------------Function get_orientados
 CREATE OR REPLACE FUNCTION get_orientados(
     orientador_input text,
     orientado_input text
