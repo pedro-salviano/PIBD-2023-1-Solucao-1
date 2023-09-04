5ae6d5f556bd15e03f6a5cafadb9759d87b39000
Daniel Hiroyuki Watanabe <35534711+dahiwas@users.noreply.github.com>
2023-09-04 16:51:01 -0300

    Adequação da view vOrientados

--- esquema_fisico/funcoes/03_view_vOrientados.sql
+++ esquema_fisico/funcoes/03_view_vOrientados.sql
@@ -3,7 +3,7 @@
 --A view retorna todas tuplas de relacionamento entre orientador e orientado mostrando se está ativo ou não
 
 -------------------------Tables necessárias:
---membro_academico
+-------------------------------Table membro_academico
 /*
 CREATE TABLE IF NOT EXISTS membro_academico(
     id SERIAL NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS membro_academico(
     PRIMARY KEY (id)
 );
 */
---orientacao
+-------------------------------Table orientacao
 /*
 CREATE TABLE IF NOT EXISTS orientacao (
     id SERIAL PRIMARY KEY,
@@ -27,7 +27,42 @@ CREATE TABLE IF NOT EXISTS orientacao (
     data_final DATE
 );
 ALTER TABLE orientacao 
-ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) REFERENCES membro_academico(identidade);
+ADD CONSTRAINT FK_especialista FOREIGN KEY (membro_academico_id) 
+REFERENCES membro_academico(identidade);
+
+ALTER TABLE orientacao 
+ADD CONSTRAINT FK_aluno_esp FOREIGN KEY (aluno_professor_isf_id) 
+REFERENCES aluno_especializacao(aluno_professor_isf_id);
+*/
+-------------------------------Table aluno_especializacao
+/*
+CREATE TABLE IF NOT EXISTS aluno_especializacao(
+	id SERIAL PRIMARY KEY,
+	aluno_professor_isf_id INTEGER NOT NULL UNIQUE, 
+    DataIngresso DATE NOT NULL,
+    DataConclusao DATE,
+    DiplomaFile VARCHAR(200),
+    Titulacao VARCHAR(200),
+    RegistroMinistrante VARCHAR(200)
+);
+
+ALTER TABLE aluno_especializacao
+        ADD CONSTRAINT FK_aluno_prof_id
+            FOREIGN KEY (aluno_professor_isf_id)
+            REFERENCES aluno_professor_isf(membro_academico_id); 
+*/
+
+-------------------------------Table aluno_professor_isf
+/*
+CREATE TABLE IF NOT EXISTS aluno_professor_isf (
+	id SERIAL PRIMARY KEY,
+    membro_academico_id INTEGER UNIQUE NOT NULL,
+    RA VARCHAR(15) NOT NULL
+);		
+ALTER TABLE aluno_professor_isf
+    ADD CONSTRAINT FK_membro_academico 
+	FOREIGN KEY (membro_academico_id) 
+	REFERENCES membro_academico(id);
 */
 -------------------------------------------------------------------------------------------------
 --viewVOrientados
@@ -44,29 +79,16 @@ FROM
 LEFT JOIN
     membro_academico o ON oe.membro_academico_id = o.identidade -- Orientadores
 LEFT JOIN
-    membro_academico a ON oe.aluno_professor_isf_id = a.identidade; -- Orientados
-
-/*
---VIEW -- COM PERIODO
-CREATE VIEW vOrientados AS
-SELECT 
-    o.identidade AS orientador_id,
-    o.nome_completo AS orientador_nome,
-    a.identidade AS orientado_id,
-    a.nome_completo AS orientado_nome,
-	TO_CHAR(CEIL(EXTRACT(MONTH FROM AGE(oe.data_final, oe.data_inicial)) / 6), '999') AS quantidade_de_periodos
-FROM 
-    orientacao oe
+    aluno_especializacao ae ON oe.aluno_professor_isf_id = ae.aluno_professor_isf_id
 LEFT JOIN
-    membro_academico o ON oe.membro_academico_id = o.identidade -- Orientadores
+    aluno_professor_isf ap ON ae.aluno_professor_isf_id = ap.membro_academico_id
 LEFT JOIN
-    membro_academico a ON oe.aluno_professor_isf_id = a.identidade; -- Orientados
-*/
+    membro_academico a ON ap.membro_academico_id = a.id; -- Orientados
 
 
 /*
---Scripts para testar
---Inserindo exemplos de pessoas
+Scripts para teste:
+------------Inserção dos membros academicos
 INSERT INTO membro_academico (identidade, nacionalidade, pais_de_residencia, nome_completo, data_nascimento, nome_da_mae, genero)
 VALUES (123456, 'Brasileira', 'Brasil', 'João da Silva', '1990-01-15', 'Mario da Silva', 'Masculino');
 
@@ -88,21 +110,36 @@ VALUES (147258, 'Brasileira', 'Brasil', 'Yukihira Akame', '1990-01-15', 'Paloma 
 INSERT INTO membro_academico (identidade, nacionalidade, pais_de_residencia, nome_completo, data_nascimento, nome_da_mae, genero)
 VALUES (785963, 'Brasileira', 'Brasil', 'Trilecas Alexandria', '1990-01-15', 'Iodeto da Silva', 'Masculino');
 
---Inserindo relações
-INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 23457, NOW());
+------------Inserindo Aluno_Professor_ISF
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), 'algumRA')
+
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), 'algumRA')
+
+INSERT INTO aluno_professor_isf(membro_academico_id, RA)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), 'algumRA')
+
+------------Inserindo Aluno_especializacao
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), NOW())
+
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), NOW())
+
+INSERT INTO aluno_especializacao(aluno_professor_isf_id, DataIngresso)
+VALUES((SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), NOW())
+
+------------ Finalmente inserindo as relações
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (89798789, 1593557, NOW());
+VALUES (123456, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Fidalgo Alexandria'), NOW());
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (89798789, 258369, NOW());
+VALUES (123456, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Hirokawa da Silva'), NOW());
 
 INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 147258, NOW());
-
-INSERT INTO orientacao (membro_academico_id, aluno_professor_isf_id, data_inicial)
-VALUES (123456, 785963, NOW());
+VALUES (785963, (SELECT MAX(id) from membro_academico WHERE nome_completo = 'Pingo Pinga'), NOW());
 
 --Exemplo da execução da view -- Visualizando todas as tuplas
 SELECT * FROM vOrientados
