708066f9056b2c9e5f65920f6ca138c2ba585321
marshall1003 <marciel.almeida.ismart@gmail.com>
2023-09-06 09:46:41 -0300

    Update 01_table_turma_especializacao.sql
    Faltava os campos do dia da semana que seria ofertada a turma e horario

--- esquema_fisico/01_table_turma_especializacao.sql
+++ esquema_fisico/01_table_turma_especializacao.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS turma_especializacao(
 	Data_Fim DATE,
 	Data_Inicio DATE NOT NULL,
 	trilha_escolhida_id INT,
+	Dia_Semana int NOT NULL,
+	Horario TEXT NOT NULL,
 	PRIMARY KEY(id)
 );
