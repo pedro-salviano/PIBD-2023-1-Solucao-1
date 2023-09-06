99009d79ed2526aa76dbc0bd298146981df6fb0b
thiago <thiagoalbino2000@hotmail.com>
2023-09-05 21:54:05 -0300

    Arrumando a prodecure

--- esquema_fisico/funcoes/03_procedure_procNovoMaterial.sql
+++ esquema_fisico/funcoes/03_procedure_procNovoMaterial.sql
@@ -1,38 +1,26 @@
--- CREATE TABLE NovoMaterial (
---   id serial PRIMARY KEY,
---   orientador VARCHAR(50) NOT NULL,
---   orientado VARCHAR(50) NOT NULL,
---   link VARCHAR(300) NOT NULL,
---   status BOOLEAN NOT NULL
--- );
-
 CREATE OR REPLACE PROCEDURE procNovoMaterial(
-    m_orientador VARCHAR(50),
-    m_orientado VARCHAR(50),
-    m_link VARCHAR(300),
-    m_status BOOLEAN
-)
-LANGUAGE plpgsql
-AS $$
+    orientador VARCHAR(50),
+    orientado VARCHAR(50),
+    link VARCHAR(300),
+    status BOOLEAN
+);
+
+RETURNS INTEGER AS $$
+DECLARE
+    material_id INTEGER;
+
 BEGIN
-    IF m_orientador IS NULL THEN
-    RAISE EXCEPTION 'O campo orientador não pode ser nulo';
-ELSIF m_orientado IS NULL THEN
-    RAISE EXCEPTION 'O campo orientado não pode ser nulo';
-ELSIF m_link IS NULL THEN
-    RAISE EXCEPTION 'O campo link não pode ser nulo';
-END IF;
+    -- Insere o registro na tabela material
+    INSERT INTO material(orientador, orientado, link, status) 
+    VALUES (orientador, orientado, link, status)
+    RETURNING id INTO material_id;
 
-    INSERT INTO NovoMaterial (orientador, orientado, link, status)
-    VALUES (m_orientador, m_orientado, m_link, m_status);
+    -- Insere o registro na tabela especialista_produz_material
+    INSERT INTO especialista_produz_material(especialista_id, material_id) 
+    VALUES (material_id);
+
+    RETURN material_id;
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
-
--- Teste 1
--- CALL procNovoMaterial('João da Silva', 'Maria da Silva', 'https://www.google.com', TRUE);
--- SELECT * FROM NovoMaterial;
-
---Teste 2
--- CALL procNovoMaterial(NULL, 'Maria da Silva', 'https://www.google.com', TRUE);
--- SELECT * FROM NovoMaterial;
+SELECT procNovoMaterial('orientador', 'orientado', 'link', 'status')
