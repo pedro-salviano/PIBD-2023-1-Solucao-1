82eeb35b5127fe33bb2e7c497729a40949215584
vgmbs1lva <135669181+vgmbs1lva@users.noreply.github.com>
2023-09-07 11:48:15 -0300

    Update 03_procedure_getTurmaOffer.sql

--- 03_procedure_getTurmaOffer.sql
+++ 03_procedure_getTurmaOffer.sql
@@ -1,12 +1,19 @@
 CREATE OR REPLACE FUNCTION public.getTurmaOffer()
-	RETURNS TABLE(
-		data_inicio DATE,
-		data_fim DATE,
-		idioma VARCHAR(20),
-		Nome_completo VARCHAR(50),
-		codigo VARCHAR(10)
-	) AS $$
+    RETURNS TABLE(
+        data_inicio DATE,
+        data_fim DATE,
+        idioma VARCHAR(20),
+        Nome_completo VARCHAR(50),
+        codigo VARCHAR(10)
+    ) AS $$
 BEGIN
-	RETURN QUERY SELECT * FROM vTurmaOffer;
+    RETURN QUERY
+    SELECT
+        data_inicio,
+        data_final AS data_fim,
+        idioma,
+        nome_completo AS Nome_completo,
+        codigo
+    FROM vTurmaOffer;
 END;
 $$ LANGUAGE plpgsql;
